9587d1ea7bbd415eeeb4212141566f7b5d128d4e
Peter William Birch <birchb1024@gmail.com>
2019-06-16 11:46:34 +1000

    Add built document

--- make.sh
+++ make.sh
@@ -3,6 +3,13 @@ set -e
 set -u
 set -x
 version=$(git describe --abbrev)
+
+if [[ "${1}" == "doc" ]]
+then
+	asciidoc README.asciidoc && mv README.html doc
+	exit
+fi
+
 go build -ldflags "-X github.com/birchb1024/goyamp.Version=${version}"
 go build -o goyamp -ldflags "-X github.com/birchb1024/goyamp.Version=${version}" cmd/main.go
 (cd test; go test -args $*)
